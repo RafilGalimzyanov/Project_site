6890f54749d4b25f950323a353c021eabc1ea5fb
RafilGalimzyanov <54663137+RafilGalimzyanov@users.noreply.github.com>
2023-04-14 13:55:37 +0300

    Базовая структура приложения

---
+++ rz_site/main_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'main_app/index.html')
+
+def work_examples(request):
+    return render(request, 'main_app/work_examples.html')
+
+def reviews(request):
+    return render(request, 'main_app/reviews.html')
+
+def articles(request):
+    return render(request, 'main_app/articles.html')
+
+def authorization(request):
+    return render(request, 'main_app/authorization.html')
