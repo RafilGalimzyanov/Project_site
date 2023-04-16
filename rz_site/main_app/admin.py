9a03be42754507f61d18c3c4cea22ad8600e36ed
RafilGalimzyanov <54663137+RafilGalimzyanov@users.noreply.github.com>
2023-04-16 11:22:05 +0300

    Доработал модели, добавил отзывы

--- rz_site/main_app/admin.py
+++ rz_site/main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from main_app.models import Articles
+from main_app.models import Articles, Review
 
 
 admin.site.register(Articles)
+admin.site.register(Review)
--- rz_site/main_app/models.py
+++ rz_site/main_app/models.py
@@ -1,8 +1,29 @@
 from django.db import models
 
+
 class Articles(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+
+class Review(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    rev = models.TextField(blank=True, verbose_name='Отзыв')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
