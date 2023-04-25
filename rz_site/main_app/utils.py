7b454397627aeabcc73955e2ce7eefaa73213bd5
RafilGalimzyanov <54663137+RafilGalimzyanov@users.noreply.github.com>
2023-04-25 19:05:27 +0300

    fix

--- rz_site/main_app/utils.py
+++
@@ -1,5 +0,0 @@
-menu = [{'title': "Главная страница", 'url': 'index'},
-        {'title': "Примеры работ", 'url': 'work_examples'},
-        {'title': "Отзывы", 'url': 'reviews'},
-        {'title': "Статьи", 'url': 'articles'},
-]
--- rz_site/main_app/views.py
+++ rz_site/main_app/views.py
@@ -9,29 +9,28 @@ from .models import Review
 
 def index(request):
     if request.user.is_authenticated:
-        return render(request, 'main_app/index.html', context={'user_info': 'Авторизован', 'menu': menu, })
-    return render(request, 'main_app/index.html', context={'menu': menu, })
+        return render(request, 'main_app/index.html')
+    return render(request, 'main_app/index.html')
 
 
 def work_examples(request):
-    return render(request, 'main_app/work_examples.html', context={'menu': menu, })
+    return render(request, 'main_app/work_examples.html')
 
 
 def reviews(request):
     all_reviews = Review.objects.all()
     context = {
         'reviews': all_reviews,
-        'menu': menu,
     }
     return render(request, 'main_app/reviews.html', context=context)
 
 
 def articles(request):
-    return render(request, 'main_app/articles.html', context={'menu': menu, })
+    return render(request, 'main_app/articles.html')
 
 
 def registration(request):
-    return render(request, 'main_app/registration.html', context={'menu': menu, })
+    return render(request, 'main_app/registration.html')
 
 
 def out_from_account(request):
